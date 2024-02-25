08e2ad63827451738159106e7e0516c9ec349d20
aziran07 <pilon1945@naver.com>
2024-02-25 02:42:47 +0000

    add static files

--- laby/src/index.js
+++ laby/src/index.js
@@ -7,10 +7,11 @@ const htmlPath = path.join(__dirName, "html");
 const app = express();
 const PORT = process.env.LABY_PORT;
 
+app.use("", express.static(path.join(htmlPath, "data")));
 app.get("/", async (_req, res) => {
   res.sendFile(path.join(htmlPath, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening...`);
+  console.log(`Server listening... ${PORT}`);
 });
