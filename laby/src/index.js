620692d869602de94d68672bf728e9c9e29fee5d
aziran07 <pilon1945@naver.com>
2024-02-25 00:09:59 +0000

    change express to fastify

---
+++ laby/src/index.js
@@ -0,0 +1,18 @@
+import Fastify from 'fastify'
+const app = Fastify({
+    logger: true
+  });
+
+const PORT = process.env.LABY_PORT;
+
+app.get('/',  (_req, res) => {
+    res.send({ hello: 'world' });
+});
+
+app.listen({ port: PORT }, (err,  address) => {
+    if (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+    console.log(`${address} Server listening...`);
+})
