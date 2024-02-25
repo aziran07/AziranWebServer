d927a22990df33974920a99480f3634a2d255eef
aziran07 <pilon1945@naver.com>
2024-02-25 01:08:05 +0000

    add eslint/prettier

--- laby/src/index.js
+++ laby/src/index.js
@@ -1,18 +1,18 @@
-import Fastify from 'fastify'
-const app = Fastify({
-    logger: true
-  });
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
 
 const PORT = process.env.LABY_PORT;
 
-app.get('/',  (_req, res) => {
-    res.send({ hello: 'world' });
+fastify.get("/", (_req, res) => {
+  res.send({ hello: "world" });
 });
 
-app.listen({ port: PORT, host: "::" }, (err,  address) => {
-    if (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
-    console.log(`${address} Server listening...`);
-})
+fastify.listen({ port: PORT, host: "::" }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`${address} Server listening...`);
+});
